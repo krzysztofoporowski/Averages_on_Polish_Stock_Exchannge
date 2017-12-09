2ee48b38dfafa80f07c1c6a452d9f501b625dd15
Krzysztof Oporowski <krzysztof.oporowski@plusnet.pl>
2017-12-09 13:28:28 +0100

    Add files via upload

---
+++ gpw.py
@@ -0,0 +1,16 @@
+#-*-coding:utf-8-*-
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+dane_spolki = pd.read_csv("c:\\Python27\\Projects\\gpw\\bzwbk.mst")
+
+print dane_spolki.head()
+
+przykladowy_czas = "19981201"
+przeliczony_czas = date.strftime(przykladowy_czas, "%Y%m%d")
+print przeliczony_czas
+
+
+
