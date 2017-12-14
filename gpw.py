3278899bb762c2df3851e124651c1bbfeb4fc0b8
Krzysztof Oporowski <krzysztof.oporowski@plusnet.pl>
2017-12-14 21:25:31 +0100

    Calculations of transaction

--- gpw.py
+++ gpw.py
@@ -15,8 +15,14 @@ def generate_signal(row):
     #Function returns 1 if the signal has been generated. The signal is defined as the Fast MA crossing from below the Slow MA
     if row["Fast_MA"] > row["Slow_MA"]:
         return 1
-    else:
-        return 0
+    elif row["Slow_MA"] > row["Fast_MA"]:
+        return -1
+def buy_stock(row):
+    #Function calculates if buy or not. The budget for one transaction is 1000 PLN.
+    if row["Signal-1"] == 1 and row["Signal-2"] == -1:
+        return (row["Open"] * -1)
+    elif row["Signal-2"] == 1 and row["Signal-1"] == -1:
+        return row["Open"]
 
 #end of functions section
 
@@ -28,8 +34,8 @@ stock_data.columns = ["Stock", "Date", "Open", "High", "Low", "Close", "Volume"]
 stock_data.set_index("Date",inplace = True)
 
 #calculating moving averages
-fast_ma_period = 5
-slow_ma_period = 21
+fast_ma_period = 50
+slow_ma_period = 100
 
 stock_data["Fast_MA"] = stock_data["Close"].rolling(window = fast_ma_period).mean()
 stock_data["Slow_MA"] = stock_data["Close"].rolling(window = slow_ma_period).mean()
@@ -39,9 +45,18 @@ stock_data_without_nan = stock_data[stock_data["Slow_MA"].notnull()]
 
 #generating signals
 stock_data_without_nan["Signal"] = stock_data_without_nan.apply(generate_signal, axis = 1)
+stock_data_without_nan["Signal-1"] = stock_data_without_nan["Signal"].shift(1)
+stock_data_without_nan["Signal-2"] = stock_data_without_nan["Signal-1"].shift(1)
+stock_data_without_nan = stock_data_without_nan[stock_data_without_nan["Signal-2"].notnull()]
+stock_data_without_nan["Buy_price"] = stock_data_without_nan.apply(buy_stock, axis = 1)
+stock_data_without_nan["Stock_number"] = stock_data_without_nan[stock_data_without_nan["Buy_price"].notnull()].apply(lambda v: int(1000 / v["Buy_price"]), axis = 1)
+stock_data_without_nan["Transaction_value"] = stock_data_without_nan["Buy_price"] * stock_data_without_nan["Stock_number"].abs() - stock_data_without_nan["Buy_price"] * stock_data_without_nan["Stock_number"].abs() * 0.0039
 
-print stock_data_without_nan.head()
-print stock_data_without_nan.tail()
+
+print stock_data_without_nan[stock_data_without_nan["Buy_price"].notnull()].tail()
+print stock_data_without_nan["Transaction_value"].sum()
+#print stock_data_without_nan[stock_data_without_nan["Signal"] == 1]
+#print stock_data_without_nan.tail(100)
 
 
 
